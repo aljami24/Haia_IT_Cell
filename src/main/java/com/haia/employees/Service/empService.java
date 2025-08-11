650abb65c76fa023d20f8eaa320333802fae6cfe
aljami <aljamihaia24@gmail.com>
2025-08-11 14:45:47 +0600

    service done

--- src/main/java/com/haia/employees/Service/empService.java
+++ src/main/java/com/haia/employees/Service/empService.java
@@ -5,6 +5,8 @@ import com.haia.employees.entity.empEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class empService {
     @Autowired
@@ -12,5 +14,25 @@ public class empService {
 
     public empEntity createEmp(empEntity empEntity) {
         return empRepo.save(empEntity);
+    }
+
+    public List<empEntity> getallEmp() {
+        return empRepo.findAll();
+    }
+
+    public empEntity getById(Integer id) {
+        return empRepo.findById(id).orElseThrow();
+    }
+
+    public void deleteEmp(Integer id) {
+        empRepo.deleteById(id);
+    }
+
+    public empEntity updatedEmp(Integer id, empEntity updated) {
+        empEntity save = empRepo.findById(id).orElseThrow();
+        save.setName(updated.getName());
+        save.setAddress(updated.getAddress());
+        save.setPhone(updated.getAddress());
+        return save;
     }
 }
