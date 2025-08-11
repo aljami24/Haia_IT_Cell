3809002f5bec8242a287cf0bcb96d4665680fbf3
aljami <aljamihaia24@gmail.com>
2025-08-11 14:34:48 +0600

    add service

--- src/main/java/com/haia/employees/Service/empService.java
+++ src/main/java/com/haia/employees/Service/empService.java
@@ -1,4 +1,16 @@
 package com.haia.employees.Service;
 
+import com.haia.employees.Repository.empRepo;
+import com.haia.employees.entity.empEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class empService {
+    @Autowired
+    private empRepo empRepo;
+
+    public empEntity createEmp(empEntity empEntity) {
+        return empRepo.save(empEntity);
+    }
 }
