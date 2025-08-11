d742c9ac5174f87c7e2c9e7ef4540926f2741fc2
aljami <aljamihaia24@gmail.com>
2025-08-11 15:42:42 +0600

    complete

--- src/main/java/com/haia/employees/Controller/empController.java
+++ src/main/java/com/haia/employees/Controller/empController.java
@@ -4,7 +4,6 @@ package com.haia.employees.Controller;
 import com.haia.employees.Service.empService;
 import com.haia.employees.entity.empEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,19 +15,29 @@ public class empController {
     public empService empservice;
 
     @PostMapping
-    public empEntity createmp (@RequestBody empEntity empentity){
+    public empEntity createmp(@RequestBody empEntity empentity) {
         return empservice.createEmp(empentity);
     }
 
 
     @GetMapping
-    public List<empEntity> getemp (){
+    public List<empEntity> getemp() {
         return empservice.getallEmp();
     }
 
     @GetMapping("{id}")
-    public empEntity getById(@PathVariable int id){
+    public empEntity getById(@PathVariable int id) {
         return empservice.getById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteEmp(@PathVariable int id) {
+        empservice.deleteEmp(id);
+    }
+
+    @PutMapping("{id}")
+    public empEntity update(@PathVariable int id, @RequestBody empEntity empEntity) {
+        return empservice.updatedEmp(id, empEntity);
     }
 
 }
